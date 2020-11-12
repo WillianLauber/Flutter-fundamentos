e4ffe6da8cc5cd472a995944ebc8999a908ccee7
willian <willianlauber@gmail.com>
2020-11-12 11:35:56 -0300

    layout com os componentes do Material Design

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -3,43 +3,62 @@ import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-      Column(
-        children: <Widget>[
-          Text('Deliver features faster', textDirection: TextDirection.ltr),
-          Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
-          Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
-          Column(
-            children: <Widget>[Text('Texto alatório', textDirection: TextDirection.ltr,)],),
-
-        ],
+    MaterialApp(home: Scaffold(
+      appBar: AppBar(title: Text('Transferências'),)  ,
+      body: Column(
+    children: <Widget>[Card(child :ListTile(
+      leading: Icon(Icons.monetization_on),
+        title: Text('1300.99'),
+        subtitle: Text('10000')
+      )),
+      Card(child :ListTile(
+          leading: Icon(Icons.monetization_on),
+        title: Text('239.99'),
+        subtitle: Text('10000')
+    ))]),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
       ),
-  );
+    )
+  ));
   //runApp(  MyApp());
 }
+class Transferencia {
+  final double valor;
+  final int numeroConta;
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  Transferencia(this.valor, this.numeroConta);
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class ListaTransferencias extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 1000)),
+          ItemTransferencia(Transferencia(200.0, 2000)),
+          ItemTransferencia(Transferencia(300.0, 3000)),
+        ]
     );
   }
 }
